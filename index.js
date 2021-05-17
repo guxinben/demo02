67c0fe56660c329af7b046172d3479691972a152
guxinben <1298662185@qq.com>
2021-05-17 10:05:57 +0800

    新增一个index.js文件

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-17 09:50:02
+ * @LastEditTime: 2021-05-17 09:50:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \demo02\index.js
+ */
